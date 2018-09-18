80f9fb6d6ba581ef480bd2530a29f48798f9abed
ireullin <cheinheng.lin@uitox.com>
2018-09-18 10:03:18 +0800

    Corrected a bug

--- src/main/kotlin/libs/json/Json.kt
+++ src/main/kotlin/libs/json/Json.kt
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 class Json(val parserInstance:ObjectMapper){
     companion object {
         fun stringify(obj:Any) = Json().stringify(obj)
-        fun parse(s:String) = Json().stringify(s)
+        fun parse(s:String) = Json().parse(s)
     }
     constructor():this(ObjectMapper())
     fun stringify(obj:Any) = parserInstance.writeValueAsString(obj)
