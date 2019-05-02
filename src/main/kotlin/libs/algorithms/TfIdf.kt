1687161b443548d40481c37130a8f34be17ad653
chienheng.lin <cheinheng.lin@uitox.com>
2019-05-02 18:28:43 +0800

    TfIdf is available

--- src/main/kotlin/libs/algorithms/TfIdf.kt
+++ src/main/kotlin/libs/algorithms/TfIdf.kt
@@ -5,10 +5,14 @@ import libs.math.logN
 
 class TfIdf(logBase:Double=10.0){
 
-    class Term(val term:String, val tfCount:Int, val dfCount:Int, val numTerms:Int, val numDocs:Int, log:(Number)->Double){
+    data class Term(val term:String, val tfidf:Double)
+
+    class VerboseTerm(val term:String, val tfCount:Int, val dfCount:Int, val numTerms:Int, val numDocs:Int, val log:(Number)->Double){
         val tf = tfCount / numTerms.toDouble()
         val idf = log(numDocs/dfCount.toDouble())
         val tfidf = tf*idf
+        override fun toString() = this.toJson()
+        fun toShort() = Term(term, tfidf)
     }
 
     private val funLog = logN(logBase)
@@ -27,14 +31,39 @@ class TfIdf(logBase:Double=10.0){
 
     fun allTerms() = docs.flatMap{ it.keys }.toSet()
 
-    fun evaluate():List<List<Term>>{
+    fun evaluateWithVerbose():List<List<VerboseTerm>>{
         return docs.map{ doc ->
+            val numTerms = doc.values.sum()
             doc.map { (term,tfCnt) ->
                 val dfCnt = docs.filter{ it.containsKey(term) }.size
-                val numTerms = doc.values.sum()
-                Term(term, tfCnt, dfCnt, numTerms, docs.size, funLog)
+                VerboseTerm(term, tfCnt, dfCnt, numTerms, docs.size, funLog)
+            }.sortedBy{ -it.tfidf }
+        }
+    }
+
+    fun evaluate():List<List<Term>>{
+        return docs.map{ doc ->
+            val numTerms = doc.values.sum()
+            doc.map { (term,tfCnt) ->
+                val dfCnt = docs.filter{ it.containsKey(term) }.size
+                VerboseTerm(term, tfCnt, dfCnt, numTerms, docs.size, funLog).toShort()
             }
         }
+    }
+
+    /**
+     * 計算整體單詞的重要性
+     * 此算法沒有依據
+     * 僅供參考
+     */
+    fun scoreTerms():List<VerboseTerm>{
+        val allTerms = allTerms()
+        val rc = allTerms.map { term->
+                val dfCnt = docs.filter{ it.containsKey(term) }.size
+                val tfCnt = docs.map{ it[term]?:0 }.sum()
+                VerboseTerm(term, tfCnt, dfCnt, allTerms.size, docs.size, funLog)
+        }.sortedBy{ -it.tfidf }
+        return rc
     }
 
     override fun toString():String {
--- src/test/kotlin/libs/algorithms/TfIdfTest.kt
+++ src/test/kotlin/libs/algorithms/TfIdfTest.kt
@@ -9,9 +9,11 @@ class TfIdfTest:TestCase() {
     fun test1() {
         val a = "john is very very tall".split(" ")
         val b = "mary is fucking very very fat".split(" ")
-        TfIdf().addDocument(a)
-                .addDocument(b)
-                .evaluate()
-                .also{ it -> println(it.toJson())}
+        val tfIdf = TfIdf().addDocument(a).addDocument(b)
+
+        println( tfIdf.scoreTerms() )
+        println( tfIdf.evaluateWithVerbose() )
+        println( tfIdf.evaluate().toJson() )
+
     }
 }
